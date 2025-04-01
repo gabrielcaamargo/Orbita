fc2e5be20e26fca67f614f7c81925df17aff52e7
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-04-01 20:21:42 -0300

    feat: setup ts alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+	plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@domain': './src/domain',
+          '@api': './src/api',
+          '@types': './src/types',
+        },
+      },
+    ],
+  ],
 };
