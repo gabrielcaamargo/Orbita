cf0646a6798c16affb3b85a92544e45dd5acdfb1
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-04-04 09:22:35 -0300

    feat: create touchableopacitybox component

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,5 +1,29 @@
-import {createBox} from '@shopify/restyle';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+
+import {
+	backgroundColor,
+	BackgroundColorProps,
+	border,
+	BorderProps,
+	createBox,
+	createRestyleComponent,
+	layout,
+	LayoutProps,
+	spacing,
+	SpacingProps,
+} from '@shopify/restyle';
 
 import {Theme} from '@theme';
 
 export const Box = createBox<Theme>();
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
+	SpacingProps<Theme> &
+	LayoutProps<Theme> &
+	BorderProps<Theme> &
+	TouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+	TouchableOpacityBoxProps,
+	Theme
+>([backgroundColor, spacing, layout, border], TouchableOpacity);
--- src/screens/Auth/SigninScreen/SigninScreen.tsx
+++ src/screens/Auth/SigninScreen/SigninScreen.tsx
@@ -1,9 +1,17 @@
-import {Box, Text} from '@components';
+import {Box, Text, TouchableOpacityBox} from '@components';
 
 export function SigninScreen() {
 	return (
 		<Box flex={1} justifyContent="center" alignItems="center">
-			<Text preset="headingMedium">SigninScreennn</Text>
+			<TouchableOpacityBox
+				backgroundColor="orbitalBlue"
+				paddingVertical="s8"
+				paddingHorizontal="s16"
+				borderRadius="s8">
+				<Text color="white" weight="Medium" preset="paragraphLarge">
+					SigninScreen
+				</Text>
+			</TouchableOpacityBox>
 		</Box>
 	);
 }
