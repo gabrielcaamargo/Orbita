01382725c6096f3d5bae0bdc094b5c3ef23b4da8
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-04-04 09:06:50 -0300

    feat: create text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,65 @@
+import {TextProps as RNTextProps} from 'react-native';
+
+import {createText, TextProps as RestyleTextProps} from '@shopify/restyle';
+
+import {Theme} from '@theme';
+
+type TextPresets =
+	| 'headingLarge'
+	| 'headingMedium'
+	| 'headingSmall'
+	| 'paragraphLarge'
+	| 'paragraphMedium'
+	| 'paragraphSmall'
+	| 'captionLarge'
+	| 'captionMedium'
+	| 'captionSmall';
+
+type FontWeight = 'Bold' | 'SemiBold' | 'Medium' | 'Regular';
+type FontFamily = 'Inter' | 'Montserrat';
+
+interface TextProps
+	extends Omit<RestyleTextProps<Theme>, 'fontWeight'>,
+		RNTextProps {
+	preset?: TextPresets;
+	weight?: FontWeight;
+	fontFamily?: FontFamily;
+	children: React.ReactNode;
+}
+
+function getFontFamilyStyles(fontFamily: FontFamily, fontWeight: FontWeight) {
+	return `${fontFamily}-${fontWeight}`;
+}
+
+const textPresets: Record<TextPresets, {fontSize: number; lineHeight: number}> =
+	{
+		headingLarge: {fontSize: 28, lineHeight: 34},
+		headingMedium: {fontSize: 24, lineHeight: 30},
+		headingSmall: {fontSize: 20, lineHeight: 26},
+		paragraphLarge: {fontSize: 16, lineHeight: 22},
+		paragraphMedium: {fontSize: 14, lineHeight: 20},
+		paragraphSmall: {fontSize: 13, lineHeight: 18},
+		captionLarge: {fontSize: 14, lineHeight: 18},
+		captionMedium: {fontSize: 12, lineHeight: 16},
+		captionSmall: {fontSize: 10, lineHeight: 14},
+	};
+
+export function Text({
+	preset = 'paragraphMedium',
+	weight = 'Regular',
+	fontFamily = 'Inter',
+	children,
+}: TextProps) {
+	const RestyleText = createText<Theme>();
+
+	const textSelectedPreset = textPresets[preset];
+
+	return (
+		<RestyleText
+			fontFamily={getFontFamilyStyles(fontFamily, weight)}
+			fontSize={textSelectedPreset.fontSize}
+			lineHeight={textSelectedPreset.lineHeight}>
+			{children}
+		</RestyleText>
+	);
+}
